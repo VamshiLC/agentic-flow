ff9e70def05e3f2044578a69b395c2ae86ed31f4
jakeer-s47 <jakeer.s@litecompute.ai>
2025-12-15 15:11:04 +0530

    Improvemnts 2

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,10 @@
 Model loaders for SAM3 and Qwen3-VL
 """
 from .sam3_loader import load_sam3_model, get_sam3_config
-from .qwen_loader import get_qwen_config, get_available_models, validate_server_connection
+from .qwen_direct_loader import Qwen3VLDirectDetector
 
 __all__ = [
     "load_sam3_model",
     "get_sam3_config",
-    "get_qwen_config",
-    "get_available_models",
-    "validate_server_connection"
+    "Qwen3VLDirectDetector"
 ]
